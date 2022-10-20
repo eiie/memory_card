bb0258f22b04699458991a4b5a97d31f7a24cbd8
eiie <eiie>
2022-10-20 11:22:11 +0200

    readme

--- sm.py
+++ sm.py
@@ -245,14 +245,3 @@ def list_cards_tag(decks_path, tag):
 #         - Description : Comparaison de date de chaque carte pour créer la
 #         liste via la fonction de comparaison (si entier >=0 populer la
 #         nouvelle liste).
-
-# Quality: The quality of recalling the answer from a scale of 0 to 5.
-# 5: perfect response.
-# 4: correct response after a hesitation.
-# 3: correct response recalled with serious difficulty.
-# 2: incorrect response; where the correct one seemed easy to recall.
-# 1: incorrect response; the correct one remembered.
-# 0: complete blackout.
-# Easiness: The easiness factor, a multipler that affects the size of the interval, determine by the quality of the recall.
-# Interval: The gap/space between your next review.
-# Repetitions: The count of correct response (quality >= 3) you have in a row.
