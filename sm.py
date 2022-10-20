2dee5acccd5d99cf08acad5bb69b0aa67fe0a6a1
eiie <eiie>
2022-10-20 11:15:41 +0200

    add create and update functions

--- sm.py
+++ sm.py
@@ -1,15 +1,62 @@
 from supermemo2 import SMTwo
 from datetime import datetime
 from os import listdir
+from os import path
 import configparser
 
 # init var (conf)
 decks_path = "decks"
+card_parameters = ['question', 'answer', 'example', 'tags']
 date_format = "%Y-%m-%d"
 
 
+# create a card in a deck
+def create_card(decks_path, deck_name, question, answer, example, tags):
+    file_path = str(decks_path) + "/" + str(deck_name) + "/" + datetime.now().strftime("%Y%m%d%H%M%S")
+    sm2_parameters = SMTwo.first_review(1)
+    card_config = configparser.ConfigParser()
+    card_config['Card'] = {'question': question,
+                           'answer': answer,
+                           'example': example,
+                           'tags': tags}
+    card_config['SM2_Parameters'] = {'easiness': sm2_parameters.easiness,
+                                     'interval': sm2_parameters.interval,
+                                     'repetitions': sm2_parameters.repetitions,
+                                     'review_date': sm2_parameters.review_date}
+    if not path.exists(file_path):
+        with open(file_path, 'w') as card_file:
+            card_config.write(card_file)
 
 
+quality = 5
+
+
+# update card parameters : question, answer, example or tag
+def update_card_parameters(card, parameter, parameter_value):
+    for c in card:
+        if parameter in card_parameters:
+            card_config = configparser.ConfigParser()
+            card_config.read(card)
+            card_config.set('Card', str(parameter), str(parameter_value))
+            with open(card, 'w') as card_file:
+                card_config.write(card_file)
+
+
+# update SMTwo parameters : easiness, interval, repetitions and review_date
+def update_sm2_parameters(card, quality):
+    for c in card:
+        card_config = configparser.ConfigParser()
+        card_config.read(c)
+        card_easiness = card_config['SM2_Parameters']['easiness']
+        card_interval = card_config['SM2_Parameters']['interval']
+        card_repetitions = card_config['SM2_Parameters']['repetitions']
+        sm2_parameters = SMTwo(float(card_easiness), int(card_interval), int(card_repetitions)).review(quality)
+        card_config.set('SM2_Parameters', 'easiness', str(sm2_parameters.easiness))
+        card_config.set('SM2_Parameters', 'interval', str(sm2_parameters.interval))
+        card_config.set('SM2_Parameters', 'repetitions', str(sm2_parameters.repetitions))
+        card_config.set('SM2_Parameters', 'review_date', str(sm2_parameters.review_date))
+        with open(c, 'w') as card_file:
+            card_config.write(card_file)
 
 
 # display a card
@@ -41,13 +88,13 @@ def is_it_review_time(card):
 
 
 # list all cards
-def list_cards_all(dir):
-    decks = listdir(dir)
+def list_cards_all(decks_path):
+    decks = listdir(decks_path)
     cards_table = []
     for d in decks:
-        cards = listdir(dir + "/" + d)
+        cards = listdir(decks_path + "/" + d)
         for c in cards:
-            c = dir + "/" + d + "/" + c
+            c = decks_path + "/" + d + "/" + c
             cards_table.append(c)
     return(cards_table)
 
@@ -56,11 +103,11 @@ deck = "deck1"
 
 
 # list all cards for a deck
-def list_cards_deck(dir, deck_name):
-    cards = listdir(dir + "/" + deck_name)
+def list_cards_deck(decks_path, deck_name):
+    cards = listdir(decks_path + "/" + deck_name)
     cards_table = []
     for c in cards:
-        c = dir + "/" + deck_name + "/" + c
+        c = decks_path + "/" + deck_name + "/" + c
         cards_table.append(c)
     return(cards_table)
 
@@ -69,38 +116,43 @@ tag = ['tag1', 'tag3']
 
 
 # list all cards for one or multiple tags
-def list_cards_tag(dir, tag):
-    decks = listdir(dir)
+def list_cards_tag(decks_path, tag):
+    decks = listdir(decks_path)
     cards_table = []
     for d in decks:
-        cards = listdir(dir + "/" + d)
+        cards = listdir(decks_path + "/" + d)
         for c in cards:
             card_config = configparser.ConfigParser()
-            card_config.read(dir + '/' + d + '/' + c)
+            card_config.read(decks_path + '/' + d + '/' + c)
             tags = card_config['Card']['tags']
             for t in tags.split(" "):
                 if t in tag and c not in cards_table:
-                    c = dir + "/" + d + "/" + c
+                    c = decks_path + "/" + d + "/" + c
                     cards_table.append(c)
     return cards_table
 
 
-print('cards_all')
-print(list_cards_all(decks_path))
-print('cards_deck')
-print(list_cards_deck(decks_path, deck))
-print('cards_tag')
-print(list_cards_tag(decks_path, tag))
+# print('cards_all')
+# print(list_cards_all(decks_path))
+# print('cards_deck')
+# print(list_cards_deck(decks_path, deck))
+# print('cards_tag')
+# print(list_cards_tag(decks_path, tag))
 
-print('is_it_review_time ? all')
-print(is_it_review_time(list_cards_all(decks_path)))
-print('is_it_review_time ? deck')
-print(is_it_review_time(list_cards_deck(decks_path, deck)))
-print('is_it_review_time ? tag')
-print(is_it_review_time(list_cards_tag(decks_path, tag)))
-print('display card')
-print(display_cards((is_it_review_time(list_cards_all(decks_path)))))
-
+# print('is_it_review_time ? all')
+# print(is_it_review_time(list_cards_all(decks_path)))
+# print('is_it_review_time ? deck')
+# print(is_it_review_time(list_cards_deck(decks_path, deck)))
+# print('is_it_review_time ? tag')
+# print(is_it_review_time(list_cards_tag(decks_path, tag)))
+# print('display card')
+# print(display_cards((is_it_review_time(list_cards_all(decks_path)))))
+# print('update sm2 parameters')
+# print(update_sm2_parameters(((is_it_review_time(list_cards_all(decks_path)))), quality))
+# print('update card parameters')
+# update_card_parameters('decks/deck1/card1', 'question', 'quetol')
+# print('create a card')
+# create_card(decks_path, "deck2", "a", "b", "c", "d")
 
 # Create a card from scratch (question, answer, tags)
 # Modify  a card (question / answer / tags (add, remove))
@@ -144,27 +196,26 @@ print(display_cards((is_it_review_time(list_cards_all(decks_path)))))
 #             - Liste des cartes (modifier)
 
 # Fonctions :
-#     - Afficher une carte
+#     + Afficher une carte
 #         - Entrée : carte à afficher
 #         - Sortie : Question, réponse, exemple
 #         - Description : Afficher une carte dans l'ordre : question puis
 #         réponse et exemple
-#     - Créer une carte
+#     + Créer une carte
 #         - Entrée : carte à modifier, paramètre à modifier, valeur du paramètre
 #         - Sortie : n/a
 #         - Description : Créer une carte avec ses paramètres : question,
-#         réponse, exemple, tag
-#     - Mettre à jour une carte (Card)
+#         réponse, exemple, tag (SM2Parameters = first_review)
+#     + Mettre à jour une carte (Card)
 #         - Entrée : carte à modifier, paramètre à modifier, valeur du paramètre
 #         - Sortie : n/a
 #         - Description : Mettre à jour une carte avec ses paramètres:
 #         question, réponse, exmple, tag
-#     - Mettre à jour une carte (SM2)
+#     + Mettre à jour une carte (SM2)
 #         - Entrée : carte à modifier, qualité (1-5)
 #         - Sortie : n/a
 #         - Description : utilisation de la fonction SMtwo pour mettre  à jour
-#         les paramètres SM2. Si la review_date est vide alors first_review
-#         sinon on récupère les paramètres SM2 de la carte en entrée de la
+#         les paramètres SM2. On récupère les paramètres SM2 de la carte en entrée de la
 #         fonction SMtwo. On utilise la date actuelle pour mettre à  jour une
 #         carte.
 #     + Vérifier date de revue et compare à la date du jour
@@ -195,49 +246,13 @@ print(display_cards((is_it_review_time(list_cards_all(decks_path)))))
 #         liste via la fonction de comparaison (si entier >=0 populer la
 #         nouvelle liste).
 
-
-
-# using default date date.today()
-a = SMTwo.first_review(1)
-b = SMTwo.first_review(2, "2022-09-22")
-c = SMTwo.first_review(3, "2022-09-22")
-d = SMTwo.first_review(4, "2022-09-22")
-e = SMTwo.first_review(5, "2022-09-22")
-
-print("first review")
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-
-# aa = SMTwo(a.easiness, a.interval, a.repetitions).review(1)
-aa = SMTwo(b.easiness, b.interval, b.repetitions).review(1, "2022-09-23")
-bb = SMTwo(b.easiness, b.interval, b.repetitions).review(2, "2022-09-23")
-cc = SMTwo(c.easiness, c.interval, c.repetitions).review(3, "2022-09-23")
-dd = SMTwo(d.easiness, d.interval, d.repetitions).review(4, "2022-09-23")
-ee = SMTwo(e.easiness, e.interval, e.repetitions).review(5, "2022-09-23")
-
-print("second review")
-print(aa)
-print(bb)
-print(cc)
-print(dd)
-print(ee)
-
-aaa = SMTwo(aa.easiness, aa.interval, aa.repetitions).review(1)
-bbb = SMTwo(bb.easiness, bb.interval, bb.repetitions).review(2, "2022-09-24")
-ccc = SMTwo(cc.easiness, cc.interval, cc.repetitions).review(3, "2022-09-29")
-ddd = SMTwo(dd.easiness, dd.interval, dd.repetitions).review(4, "2022-09-29")
-eee = SMTwo(ee.easiness, ee.interval, ee.repetitions).review(5, "2022-09-24")
-
-print("third review")
-print(aaa)
-print(bbb)
-print(ccc)
-print(ddd)
-print(eee)
-
-print("review date")
-print(aaa.review_date)
-print(bbb.review_date)
+# Quality: The quality of recalling the answer from a scale of 0 to 5.
+# 5: perfect response.
+# 4: correct response after a hesitation.
+# 3: correct response recalled with serious difficulty.
+# 2: incorrect response; where the correct one seemed easy to recall.
+# 1: incorrect response; the correct one remembered.
+# 0: complete blackout.
+# Easiness: The easiness factor, a multipler that affects the size of the interval, determine by the quality of the recall.
+# Interval: The gap/space between your next review.
+# Repetitions: The count of correct response (quality >= 3) you have in a row.
